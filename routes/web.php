01a6288c95330d7ac7856cc4e28e0ca8c74f6f44
Yago Gomes <yagogomesc@gmail.com>
2019-03-12 12:49:29 -0300

    Update web.php
    Aulas 12, 13 e 14, criação de rota e rota com regra

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/nome/{nome}/{sobrenome}', function($nome, $sn){
+    return "<h1>Ola $nome $sn!</h1>";
+});
+
+Route::get('/repetir/{nome}/{n}', function ($nome, $n){
+  for($i=0; $i<$n; $i++){
+      echo "<h1>Ola, $nome!</h1>";
+  }
+});
+
+Route::get('/nomecomregra/{nome}/{n}', function($nome, $n){
+  for($i=0; $i<$n; $i++){
+      echo "<h1>Ola, $nome!</h1>";
+  }
+})->where('n', '[0-9]+');

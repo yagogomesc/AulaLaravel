328a0b5d7e5680251bbc3f582e35663650b27e8c
Yago Gomes <yagogomesc@gmail.com>
2019-03-22 12:48:30 -0300

    Parametros para componentes
    Aula 36 - Passando parametro para componentes

--- views/resources/views/pagina.blade.php
+++ views/resources/views/pagina.blade.php
@@ -5,7 +5,11 @@
 </head>
 <body>
 
-  @component('components.meucomponente')
+  @component('components.meucomponente',['tipo'=>'danger', 'titulo' => 'Erro Fatal'])
+    <strong>Erro: </strong> Sua mensagem de erro.
+  @endcomponent
+
+  @component('components.meucomponente',['tipo'=>'warning', 'titulo' => 'Erro Quase Fatal'])
     <strong>Erro: </strong> Sua mensagem de erro.
   @endcomponent
 

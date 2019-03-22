4f3326efbbe03b2c6d0c07594b5fbca7c34b0f24
Yago Gomes <yagogomesc@gmail.com>
2019-03-22 12:23:22 -0300

    Criando componente
    Aula 35 - Criando um componente e como utilizalo

--- views/resources/views/pagina.blade.php
+++ views/resources/views/pagina.blade.php
@@ -4,7 +4,10 @@
   <link href="{{asset('css/app.css')}}" rel="stylesheet">
 </head>
 <body>
-  <div class="alert alert-primary" role="alert">A simple primary alert-check it out!</div>
+
+  @component('components.meucomponente')
+    <strong>Erro: </strong> Sua mensagem de erro.
+  @endcomponent
 
   <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
 </body>

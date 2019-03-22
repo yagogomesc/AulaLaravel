204fd4a7836f31666220b33eaf654dd262569105
Yago Gomes <yagogomesc@gmail.com>
2019-03-22 11:55:08 -0300

    Inserindo bootstrap ao projeto
    Aula 34 - Inserindo bootstrap no projeto para desenvolvimento de layout e componentes

---
+++ views/resources/views/pagina.blade.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+  <title></title>
+  <link href="{{asset('css/app.css')}}" rel="stylesheet">
+</head>
+<body>
+  <div class="alert alert-primary" role="alert">A simple primary alert-check it out!</div>
+
+  <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
+</body>
+</html>
--- views/routes/web.php
+++ views/routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('filho');
+    return view('pagina');
 });
 
 Route::get('/primeiraview', function(){

36c97bf63b7c65b879bf285cf4c8f3dd2ee41617
Yago Gomes <yagogomesc@gmail.com>
2019-03-23 20:28:34 -0300

    Update foreach.blade.php
    Aula 42 - Atributos dos laços de repeticao

--- views/resources/views/foreach.blade.php
+++ views/resources/views/foreach.blade.php
@@ -1,3 +1,7 @@
+@extends('layout.meulayout')
+
+@section('minha_secao_produtos')
+
 <h1>Loop Foreach - Arrays Associativos</h1>
 
 @foreach($produtos as $p)
@@ -5,3 +9,23 @@
     <p>{{$p['id']}}: {{$p['nome']}}</p>
 
 @endforeach
+
+<hr>
+
+@foreach($produtos as $p)
+
+    <p>
+      {{$p['id']}}: {{$p['nome']}}
+      @if($loop->first)
+        (primeiro)
+      @endif
+      @if($loop->last)
+        (ultimo)
+      @endif
+      <span class="badge badge-secondary">{{$loop->index+1}} / {{$loop->count}} / {{$loop->remaining}}</span>
+      <span class="badge badge-warning">{{$loop->iteration}} / {{$loop->count}}</span>
+    </p>
+
+@endforeach
+
+@endsection

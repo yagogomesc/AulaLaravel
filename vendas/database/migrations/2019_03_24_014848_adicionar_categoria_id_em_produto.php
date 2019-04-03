5f5b3a44b5178247e43cd445078a518a6f74557d
Yago Gomes <yagogomesc@gmail.com>
2019-04-03 12:41:28 -0300

    Ajustes e testes de seeder

--- vendas/database/migrations/2019_03_24_014848_adicionar_categoria_id_em_produto.php
+++ vendas/database/migrations/2019_03_24_014848_adicionar_categoria_id_em_produto.php
@@ -14,7 +14,7 @@ class AdicionarCategoriaIdEmProduto extends Migration
     public function up()
     {
         Schema::table('produtos', function (Blueprint $table) {
-            $table->unsignedBigInteger('categoria_id')->unsigned();
+            $table->BigInteger('categoria_id')->unsigned();
             $table->foreign('categoria_id')->references('id')->on('categorias');
         });
     }
--- vendas/database/migrations/2019_03_25_150541_create_table_test.php
+++ vendas/database/migrations/2019_03_25_150541_create_table_test.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Teste extends Migration
+class CreateTableTest extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,13 @@ class Teste extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('teste', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->float('valor');
+            $table->integer('quantidade');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +29,6 @@ class Teste extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('teste');
     }
 }

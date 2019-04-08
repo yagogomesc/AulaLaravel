ba295a3d7037f20bb390cdcc8519d18c820b07b0
Yago Gomes <yagogomesc@gmail.com>
2019-04-08 12:15:16 -0300

    Retornando categoria por ID
    Aula 66 - Retornando categoria por ID

--- modelos/routes/web.php
+++ modelos/routes/web.php
@@ -16,3 +16,14 @@ Route::get('/inserir/{nome}', function($nome){
     $cat->save();
     return redirect('/');
 });
+//aula 66
+Route::get('/categoria/{id}', function($id){
+    $cat = Categoria::findOrFail($id);
+    if(isset($cat)):
+      echo "id: " . $cat->id.", ";
+      echo "nome: " . $cat->nome."<br>";
+    else:
+      echo "<h1>Categoria não encontrada</h1>";
+    endif;
+    //return redirect('/');
+});

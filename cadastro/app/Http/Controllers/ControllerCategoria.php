8d67da1ed46680f24385d2bdba699439c433de2b
Yago Gomes <yagogomesc@gmail.com>
2019-04-24 13:14:11 -0300

    Pagina de categorias
    Aula 84 - Exibindo categorias

--- cadastro/app/Http/Controllers/ControllerCategoria.php
+++ cadastro/app/Http/Controllers/ControllerCategoria.php
@@ -14,7 +14,8 @@ class ControllerCategoria extends Controller
      */
     public function index()
     {
-        return view('categorias');
+        $cats = Categoria::all();
+        return view('categorias', compact('cats'));
     }
 
     /**
@@ -38,6 +39,7 @@ class ControllerCategoria extends Controller
         $cat = new Categoria();
         $cat->nome = $request->input('nomeCategoria');
         $cat->save();
+        return redirect('/categorias');
     }
 
     /**
--- cadastro/resources/views/categorias.blade.php
+++ cadastro/resources/views/categorias.blade.php
@@ -1,5 +1,37 @@
 @extends('layout.app', ["current" => "categorias"])
 
 @section('body')
-    <h4>Página de categorias</h4>
+    <div class="card border">
+      <div class="card-body">
+        <h5 class="card-title">Categorias</h5>
+@if(count($cats) > 0)
+        <table class="table table-ordered table-hover">
+          <thead>
+            <tr>
+              <th>Código</th>
+              <th>Nome da Categoria</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($cats as $cat)
+
+              <tr>
+                <td>{{$cat->id}}</td>
+                <td>{{$cat->nome}}</td>
+                <td>
+                  <a href="/AulaLaravel/cadastro/public/categorias/editar/{{$cat->id}}" class="btn btn-sm btn-primary">Editar</a>
+                  <a href="/AulaLaravel/cadastro/public/categorias/apagar/{{$cat->id}}" class="btn btn-sm btn-danger">Apagar</a>
+                </td>
+              </tr>
+
+            @endforeach
+          </tbody>
+        </table>
+@endif
+      </div>
+      <div class="card-footer">
+        <a href="/AulaLaravel/cadastro/public/categorias/novo" class="btn btn-sm btn-primary" role="button">Nova categoria</a>
+      </div>
+    </div>
 @endsection

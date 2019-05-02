46dd19fc7dfda43a7c9748c2f1d17ed91585ccec
Yago Gomes <yagogomesc@gmail.com>
2019-05-02 12:44:22 -0300

    Preparando o ambiente
    Aula 90

---
+++ validacao/resources/views/novocliente.blade.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+  <title>Cadastro de Cliente</title>
+  <link href="{{asset('css/app.css')}}" rel="stylesheet">
+  <meta name="csrf-token" content="{{csrf_token()}}">
+</head>
+<body>
+
+    <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
+</body>
+</html>

f0d077f2eca75fb647362120ef8db3798530810f
Yago Gomes <yagogomesc@gmail.com>
2019-05-03 12:37:19 -0300

    Salvando clientes
    Aula 94

--- validacao/app/Http/Controllers/ClienteControlador.php
+++ validacao/app/Http/Controllers/ClienteControlador.php
@@ -36,7 +36,14 @@ class ClienteControlador extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $cliente = new Cliente();
+        $cliente->nome = $request->input('nome');
+        $cliente->idade = $request->input('idade');
+        $cliente->endereco = $request->input('endereco');
+        $cliente->email = $request->input('email');
+        $cliente->save();
+
+        redirect("/");
     }
 
     /**
--- validacao/routes/web.php
+++ validacao/routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', 'ClienteControlador@index');
 
 Route::get('/novocliente', 'ClienteControlador@create');
+
+Route::post('/cliente', 'ClienteControlador@store');

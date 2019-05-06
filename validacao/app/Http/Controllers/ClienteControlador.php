d7b7c81ccc072c7c7517d6c1eb5c684ba849a46e
Yago Gomes <yagogomesc@gmail.com>
2019-05-06 13:58:06 -0300

    Aula 99 - Personalizando avisos de erro

--- validacao/app/Http/Controllers/ClienteControlador.php
+++ validacao/app/Http/Controllers/ClienteControlador.php
@@ -36,12 +36,31 @@ class ClienteControlador extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
+
+      $regras = [
+          'nome' => 'required|min:3|max:20|unique:clientes',
+          'idade' => 'required',
+          'endereco' => 'required|min:5',
+          'email' => 'required|email'
+      ];
+
+      $mensagens = [
+          'required' => 'O campo :attribute não pode estar em branco',
+          'nome.required' => 'O nome é requerido',
+          'nome.min' => 'É necessário no minimo 3 caracteres no nome.',
+          'email.required' => 'Digite um endereço de email.',
+          'email.email' => 'Digite um endereço de email válido'
+      ];
+      $request->validate($regras, $mensagens);
+
+
+        /*$request->validate([
             'nome' => 'required|min:3|max:20|unique:clientes',
             'idade' => 'required',
             'endereco' => 'required|min:5',
             'email' => 'required|email'
-        ]);
+        ]);*/
+
         $cliente = new Cliente();
         $cliente->nome = $request->input('nome');
         $cliente->idade = $request->input('idade');

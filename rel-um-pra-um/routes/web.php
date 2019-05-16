4e1314a42f28d09274d1f47fa1bb54bad8c89752
Yago Gomes <yagogomesc@gmail.com>
2019-05-15 22:58:30 -0300

    Aula 122 - Listando clientes e endereços

--- rel-um-pra-um/routes/web.php
+++ rel-um-pra-um/routes/web.php
@@ -11,6 +11,39 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Cliente;
+use App\Endereco;
+
+Route::get('/clientes', function () {
+    $clientes = Cliente::all();
+    foreach($clientes as $c):
+      echo "<p>ID: " . $c->id . "</p>";
+      echo "<p>Nome: " . $c->nome . "</p>";
+      echo "<p>Telefone: " . $c->telefone . "</p>";
+      $e = Endereco::where('cliente_id', $c->id)->first();
+
+      echo "<p>Rua: " . $e->rua . "</p>";
+      echo "<p>Numero: " . $e->numero . "</p>";
+      echo "<p>Bairro: " . $e->bairro . "</p>";
+      echo "<p>Cidade: " . $e->cidade . "</p>";
+      echo "<p>UF: " . $e->uf . "</p>";
+      echo "<p>CEP: " . $e->cep . "</p>";
+
+      echo "<hr>";
+    endforeach;
+});
+
+Route::get('/enderecos', function () {
+    $ends = Endereco::all();
+    foreach($ends as $e):
+      echo "<p>ID: " . $e->cliente_id . "</p>";
+      echo "<p>Rua: " . $e->rua . "</p>";
+      echo "<p>Numero: " . $e->numero . "</p>";
+      echo "<p>Bairro: " . $e->bairro . "</p>";
+      echo "<p>Cidade: " . $e->cidade . "</p>";
+      echo "<p>UF: " . $e->uf . "</p>";
+      echo "<p>CEP: " . $e->cep . "</p>";
+
+      echo "<hr>";
+    endforeach;
 });

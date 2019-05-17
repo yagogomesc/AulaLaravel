9180076d670335dfad280a0027b57bf606693d2c
Yago Gomes <yagogomesc@gmail.com>
2019-05-16 21:03:20 -0300

    Aula 132 - Utilizando belongsTo

--- rel-um-pra-muitos/app/Produto.php
+++ rel-um-pra-muitos/app/Produto.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    //
+    public function categoria(){
+
+        return $this->belongsTo('App\Categoria');
+
+    }
 }
--- rel-um-pra-muitos/routes/web.php
+++ rel-um-pra-muitos/routes/web.php
@@ -11,6 +11,35 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Produto;
+use App\Categoria;
+
+Route::get('/categorias', function () {
+    $cat = Categoria::all();
+
+    foreach($cat as $c){
+
+        echo "<p>" . $c->id . " - " . $c->nome . "</p>";
+
+    }
+});
+
+Route::get('/produtos', function () {
+    $prod = Produto::all();
+
+    echo "<table>";
+    echo "<thead><tr><td>Nome</td>
+    <td>Estoque</td>
+    <td>Preco</td>
+    <td>Categoria</td></tr></thead>";
+
+    foreach($prod as $p){
+        echo "<tr>";
+        echo "<td>" . $p->nome . "</td>";
+        echo "<td>" . $p->estoque . "</td>";
+        echo "<td>" . $p->preco . "</td>";
+        echo "<td>" . $p->categoria->nome . "</td>";
+        echo "</tr>";
+
+    }
 });

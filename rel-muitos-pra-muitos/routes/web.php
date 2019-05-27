dc618a0b6588a1d9274c9fb702668697662fe0b8
Yago Gomes <yagogomesc@gmail.com>
2019-05-27 10:29:29 -0300

    Aula 144 - Alocando desenvolvedores a projetos

--- rel-muitos-pra-muitos/routes/web.php
+++ rel-muitos-pra-muitos/routes/web.php
@@ -61,3 +61,14 @@ Route::get('/projetos', function(){
     }
     //return $projetos->toJson();
 });
+
+Route::get('/alocar ', function(){
+    $proj = Projeto::find(4);
+    if(isset($proj)){
+        //$proj->desenvolvedores()->attach(1, ['horas_semanais' =>50 ]);
+        $proj->desenvolvedores()->attach([
+          2 => ['horas_semanais' => 20],
+          3 => ['horas_semanais' => 40],
+        ]);
+    }
+});

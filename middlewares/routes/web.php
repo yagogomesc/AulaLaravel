56a5ebf2c7ad860b1607bb9301e21e3c10c67b4f
Yago Gomes <yagogomesc@gmail.com>
2019-05-28 10:13:10 -0300

    Aula 148 - Registrando nome para middleware

--- middlewares/routes/web.php
+++ middlewares/routes/web.php
@@ -13,4 +13,4 @@
 
 use App\Http\Middleware\PrimeiroMiddleware;
 
-Route::get('/usuarios', 'UsuarioControlador@index')->middleware(PrimeiroMiddleware::class);
+Route::get('/usuarios', 'UsuarioControlador@index')->middleware('primeiro');

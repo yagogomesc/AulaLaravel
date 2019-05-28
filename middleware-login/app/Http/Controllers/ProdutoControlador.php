ab84ae4ee02cd5942352106e919a123804835a08
Yago Gomes <yagogomesc@gmail.com>
2019-05-28 11:18:26 -0300

    Aula 154 - middleware login

---
+++ middleware-login/app/Http/Controllers/ProdutoControlador.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutoControlador extends Controller
+{
+    private $produtos = ["Televisao 40", "Notebook Acer", "Impressora HP", "HD externo"];
+    public function index(){
+
+        echo "<h3>Produtos</h3>";
+        echo "<ol>";
+          foreach($this->produtos as $p){
+            echo "<li>". $p . "</li>";
+          }
+        echo "</ol>";
+
+    }
+}

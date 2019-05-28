d4d7134d10cda00d29c588bc8e11b9e7291608ed
Yago Gomes <yagogomesc@gmail.com>
2019-05-28 11:27:22 -0300

    Aula 155 - login basico por middleware

--- middleware-login/routes/web.php
+++ middleware-login/routes/web.php
@@ -10,9 +10,30 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/produtos', 'ProdutoControlador@index');
+
+Route::post('/login', function(Request $req){
+    $login_ok = false;
+    switch($req->input('user')){
+        case 'joao':
+          $login_ok = $req->input('passwd') === "senhajoao";
+        break;
+        case 'marcos':
+          $login_ok = $req->input('passwd') === "senhamarcos";
+        break;
+        default:
+        $login_ok = false;
+        break;
+    }
+    if($login_ok){
+      return response("Login OK", 200);
+    }else{
+      return response("Erro no login", 404);
+    }
+});

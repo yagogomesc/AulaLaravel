530b7a80f8497d0043b51cd5ba545df20ac02e14
Yago Gomes <yagogomesc@gmail.com>
2019-05-30 10:42:23 -0300

    Aula 162 - Redirecionando login para outra pagina

--- login-basico/routes/web.php
+++ login-basico/routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/produtos', 'ProdutoControlador@index')->middleware('auth')->name('produtos');
+Route::get('/produtos', 'ProdutoControlador@index')->name('produtos');

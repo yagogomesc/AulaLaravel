470330c67d1c6a008a05c111fcd220715ba6c028
Yago Gomes <yagogomesc@gmail.com>
2019-05-31 09:07:12 -0300

    Aula 170 - Criando controller e view

--- multiusuario/resources/views/admin.blade.php
+++ multiusuario/resources/views/admin.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard - Usuario</div>
+                <div class="card-header">Dashboard - Administrador</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    Você está logado como USUARIO!
+                    Você esta logado como ADMINISTRADOR!
                 </div>
             </div>
         </div>

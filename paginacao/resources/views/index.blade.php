b09ee7c67bf33d9b4944d32c057a246565f5a08d
Yago Gomes <yagogomesc@gmail.com>
2019-06-04 10:36:07 -0300

    Aula 183 - Criando a tabela

--- paginacao/resources/views/index.blade.php
+++ paginacao/resources/views/index.blade.php
@@ -4,14 +4,58 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{csrf_token()}}" >
+        <link href="{{asset('css/app.css')}}" rel="stylesheet">
         <title>Paginação</title>
 
     </head>
     <body>
         <div class="container">
+            <div class="card text-center">
+              <div class="card-header">Tabela de clientes</div>
+              <div class="card-body">
+                <h5 class="card-title">Exibindo 10 clientes de 1000(1 a 10)</h5>
+                <table class="table table-hover">
+                  <thead>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Sobrenome</th>
+                    <th scope="col">Email</th>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Cesar</td>
+                      <td>Augusto</td>
+                      <td>cesar@gmail.com</td>
+                    </tr>
+                    <tr>
+                      <td>1</td>
+                      <td>Cesar</td>
+                      <td>Augusto</td>
+                      <td>cesar@gmail.com</td>
+                    </tr>
+                    <tr>
+                      <td>1</td>
+                      <td>Cesar</td>
+                      <td>Augusto</td>
+                      <td>cesar@gmail.com</td>
+                    </tr>
+                    <tr>
+                      <td>1</td>
+                      <td>Cesar</td>
+                      <td>Augusto</td>
+                      <td>cesar@gmail.com</td>
+                    </tr>
 
+                  </tbody>
+                </table>
+              </div>
+              <div class="card-footer">
+                  Paginas
+              </div>
+            </div>
         </div>
-        
+
         <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
     </body>
 </html>

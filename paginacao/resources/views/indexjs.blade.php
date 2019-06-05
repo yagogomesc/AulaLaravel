6e19bade7233a76cdab10d6791700b1fb0434204
Yago Gomes <yagogomesc@gmail.com>
2019-06-05 09:35:12 -0300

    Aula 191 - paginação com javascript, inserindo paginação

--- paginacao/resources/views/indexjs.blade.php
+++ paginacao/resources/views/indexjs.blade.php
@@ -14,9 +14,9 @@
               <div class="card-header">Tabela de clientes</div>
               <div class="card-body">
                 <h5 class="card-title">
-                  
+
                 </h5>
-                <table class="table table-hover">
+                <table class="table table-hover" id="tabelaClientes">
                   <thead>
                     <th scope="col">#</th>
                     <th scope="col">Nome</th>
@@ -36,11 +36,79 @@
                 </table>
               </div>
               <div class="card-footer">
+                <nav id="paginator">
+                  <ul class="pagination">
+                    <!--
+                    <li class="page-item disabled">
+                      <a class="page-link" href="#" tabindex="-1">Previous</a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item active">
+                      <a class="page-link" href="#">2 </a>
+                    </li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item">
+                      <a class="page-link" href="#">Next</a>
+                    </li>-->
+                  </ul>
+                </nav>
 
               </div>
             </div>
         </div>
 
         <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
+
+        <script type="text/javascript">
+
+          function getItem(data, i){
+
+            if(i == data.current_page){
+                s = '<li class="page-item active">';
+            }else{
+                s = '<li class="page-item">';
+            }
+            s += '<a class="page-link" href="#">'+ i +'</a></li>';
+            return s;
+          }
+
+          function montarPaginator(data){
+              for(i=1; i<data.total;i++){
+                  s = getItem(data, i);
+                  $("#paginator>ul").append(s);
+              }
+          }
+
+          function montarLinha(cliente){
+
+            return '<tr>' +
+              '<td>' + cliente.id + '</td>' +
+              '<td>' + cliente.nome + '</td>' +
+              '<td>' + cliente.sobrenome + '</td>' +
+              '<td>' + cliente.email + '</td>' +
+            '</tr>'
+
+          }
+
+          function montarTabela(data){
+              $("#tabelaClientes>tbody>tr").remove();
+
+              for(i=0;i<data.data.length; i++){
+                  s = montarLinha(data.data[i]);
+                  $("#tabelaClientes>tbody").append(s);
+              }
+          }
+
+          function carregarClientes(pagina){
+            $.get('/json', {page: pagina}, function(resp){
+                console.log(resp);
+                montarTabela(resp);
+                montarPaginator(resp);
+            })
+          }
+          $(function(){
+              carregarClientes(2);
+          });
+        </script>
     </body>
 </html>

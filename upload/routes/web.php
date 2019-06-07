da59b7937c28a270e5b05d907b6125e2f699827a
Yago Gomes <yagogomesc@gmail.com>
2019-06-07 09:50:22 -0300

    Finalizando aulas sobre upload

--- upload/routes/web.php
+++ upload/routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PostControlador@index');
+
+Route::post('/', 'PostControlador@store');
+
+Route::delete('/{id}', 'PostControlador@destroy');
+
+Route::get('/download/{id}', 'PostControlador@download');

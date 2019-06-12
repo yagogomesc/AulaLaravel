fcdd445fa6565899b16cacf20b795bfcde459591
Yago Gomes <yagogomesc@gmail.com>
2019-06-12 10:59:12 -0300

    Aula 226 e 227 - Implementando a API

--- lar_angular/routes/api.php
+++ lar_angular/routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', 'PostControlador@index');
+
+Route::post('/', 'PostControlador@store');
+
+Route::delete('/{id}', 'PostControlador@destroy');
+
+Route::get('/like/{id}', 'PostControlador@like');
